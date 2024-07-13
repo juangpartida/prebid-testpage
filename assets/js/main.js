d62d1e78b17346227dd7e7fe5a28897c4922f630
just juan <juangpartida@protonmail.com>
2024-07-12 17:05:38 -0700

    commented 3/4 adunits for fine tuning

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,108 +261,108 @@ var adUnits = [
             }
         ]
     },
-    {
-        code: 'ad-div-160x600',
-        mediaTypes: {
-            banner: {
-                sizes: [[160, 600]]
-            }
-        },
-        bids: [
-            {
-                bidder: 'viant',
-                params: {
-                    placementId: '23469191',
-                    publisherId: '12345'
-                },
-                ortb2Imp: {
-                    pmp: {
-                        private_auction: 1,
-                        deals: [
-                            {
-                                id: '1234567',
-                                at: 3,
-                                bidfloor: 25,
-                                bidfloorcur: 'USD',
-                                ext: {
-                                    must_bid: 1,
-                                    private_auction: 1
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        ]
-    },
-    {
-        code: 'ad-div-728x90',
-        mediaTypes: {
-            banner: {
-                sizes: [[728, 90]]
-            }
-        },
-        bids: [
-            {
-                bidder: 'viant',
-                params: {
-                    placementId: '23469191',
-                    publisherId: '12345'
-                },
-                ortb2Imp: {
-                    pmp: {
-                        private_auction: 1,
-                        deals: [
-                            {
-                                id: '1234567',
-                                at: 3,
-                                bidfloor: 25,
-                                bidfloorcur: 'USD',
-                                ext: {
-                                    must_bid: 1,
-                                    private_auction: 1
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        ]
-    },
-    {
-        code: 'ad-div-300x600',
-        mediaTypes: {
-            banner: {
-                sizes: [[300, 600]]
-            }
-        },
-        bids: [
-            {
-                bidder: 'viant',
-                params: {
-                    placementId: '23469191',
-                    publisherId: '12345'
-                },
-                ortb2Imp: {
-                    pmp: {
-                        private_auction: 1,
-                        deals: [
-                            {
-                                id: '1234567',
-                                at: 3,
-                                bidfloor: 25,
-                                bidfloorcur: 'USD',
-                                ext: {
-                                    must_bid: 1,
-                                    private_auction: 1
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        ]
-    }
+//     {
+//         code: 'ad-div-160x600',
+//         mediaTypes: {
+//             banner: {
+//                 sizes: [[160, 600]]
+//             }
+//         },
+//         bids: [
+//             {
+//                 bidder: 'viant',
+//                 params: {
+//                     placementId: '23469191',
+//                     publisherId: '12345'
+//                 },
+//                 ortb2Imp: {
+//                     pmp: {
+//                         private_auction: 1,
+//                         deals: [
+//                             {
+//                                 id: '1234567',
+//                                 at: 3,
+//                                 bidfloor: 25,
+//                                 bidfloorcur: 'USD',
+//                                 ext: {
+//                                     must_bid: 1,
+//                                     private_auction: 1
+//                                 }
+//                             }
+//                         ]
+//                     }
+//                 }
+//             }
+//         ]
+//     },
+//     {
+//         code: 'ad-div-728x90',
+//         mediaTypes: {
+//             banner: {
+//                 sizes: [[728, 90]]
+//             }
+//         },
+//         bids: [
+//             {
+//                 bidder: 'viant',
+//                 params: {
+//                     placementId: '23469191',
+//                     publisherId: '12345'
+//                 },
+//                 ortb2Imp: {
+//                     pmp: {
+//                         private_auction: 1,
+//                         deals: [
+//                             {
+//                                 id: '1234567',
+//                                 at: 3,
+//                                 bidfloor: 25,
+//                                 bidfloorcur: 'USD',
+//                                 ext: {
+//                                     must_bid: 1,
+//                                     private_auction: 1
+//                                 }
+//                             }
+//                         ]
+//                     }
+//                 }
+//             }
+//         ]
+//     },
+//     {
+//         code: 'ad-div-300x600',
+//         mediaTypes: {
+//             banner: {
+//                 sizes: [[300, 600]]
+//             }
+//         },
+//         bids: [
+//             {
+//                 bidder: 'viant',
+//                 params: {
+//                     placementId: '23469191',
+//                     publisherId: '12345'
+//                 },
+//                 ortb2Imp: {
+//                     pmp: {
+//                         private_auction: 1,
+//                         deals: [
+//                             {
+//                                 id: '1234567',
+//                                 at: 3,
+//                                 bidfloor: 25,
+//                                 bidfloorcur: 'USD',
+//                                 ext: {
+//                                     must_bid: 1,
+//                                     private_auction: 1
+//                                 }
+//                             }
+//                         ]
+//                     }
+//                 }
+//             }
+//         ]
+//     }
 ];
 
 // Ensure pbjs object is initialized correctly
